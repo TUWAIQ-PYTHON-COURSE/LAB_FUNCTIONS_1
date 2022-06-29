a8ae92cc0ff4e5be49bd7d9e6b127a7ac602c228
nora <nora_abdullah33@outlook.com>
2022-06-29 19:17:41 +0300

    fixed it

--- lap3.py
+++ lap3.py
@@ -2,8 +2,8 @@
 def printPattern( number : int ):
     ''' This function takes one number then print the numbers that lead the number in upside parmiad '''
     rowsw = number
-    for i in range(rowsw, 1, -1):
-        for j in range(1, i + 1):
+    for i in range( rowsw ,0 , -1):
+        for j in range( 1,i + 1):
           print(j, end=' ')  
         print() 
 
