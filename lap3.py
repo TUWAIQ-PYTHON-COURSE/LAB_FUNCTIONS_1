b6306e9cd23279fa6ce1beacbbdf644a8ece29fe
nora <nora_abdullah33@outlook.com>
2022-06-29 19:05:23 +0300

    i did the second Q

--- lap3.py
+++ lap3.py
@@ -1,7 +1,8 @@
 
 def printPattern( number : int ):
-     rowsw = number
-     for i in range(rowsw, 1, -1):
+    ''' This function takes one number then print the numbers that lead the number in upside parmiad '''
+    rowsw = number
+    for i in range(rowsw, 1, -1):
         for j in range(1, i + 1):
           print(j, end=' ')  
         print() 
@@ -9,3 +10,6 @@ def printPattern( number : int ):
 
 
 printPattern(5)
+
+print(printPattern.__doc__)
+
