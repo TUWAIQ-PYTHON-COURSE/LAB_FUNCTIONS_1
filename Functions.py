2f5450ec3bb09fda18e256654c45b771d77ac907
Ahad <ahad@Ahads-Mac.local>
2022-06-29 19:06:58 +0300

    Function lab1

--- Functions.py
+++ Functions.py
@@ -1,28 +1,9 @@
 #[1]
-'''
 def shape(num:int):
-  #  x =""
-    for i in range(num ,1,-1):
-        for j in range (0,i):
-            print(num, end=' ')
-    print("\r")
-
-
-shape(5)
-'''
-
-
-
-def shape(num:int):
+    ''' The function shape will print the numbers in hierarchically descending'''
     for i in range(0, num + 1):
         for j in range(num - i, 0, -1):
              print(j, end=' ')
         print()
 
 shape(5)
-'''
-for i in range(0, rows + 1):
-    for j in range(rows - i, 0, -1):
-        print(j, end=' ')
-    print()
-'''
